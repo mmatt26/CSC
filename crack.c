a91f402fc0101f3eadb5db82f183c852c357bca9
mmatt26 <mmatt26@lsu.edu>
2016-10-27 12:14:33 -0500

    Update crack.c

--- crack.c
+++ crack.c
@@ -32,19 +32,24 @@ int main() {
 
   int N=pow(26, 4);
   int M=pow(26, 2);
-
+  int t;
+  
   int fd  = open("hash", 'r');
   int num = read(fd, target, 16);
   if (num) target[num-1] = 0;
   else     exit(0);
   close(fd);
-
-  for (int i=0; i<N; i++) {
+  for (t=0; t<NUM; t++)
+    pthread_create(&tid[t], &attr, hash, password);
     ith(password, i, 4);
-    for (int j=0; j<M; j++) {
-      ith(salt,     j, 2);
-      hash = crypt(password, salt);
-      if (0==strcmp(hash, target)) {
+  for (int j=0; j<M; j++) {
+    ith(salt,     j, 2);
+    for (int i=0; i<N; i++) {
+        pthread_mutex_lock(&mutex);
+        hash = crypt(password, salt);
+        copy = tid[i];
+        pthread_equal(copy,tid);
+        if (0==strcmp(hash, target)) {
         printf("Password: %s\t\tSalt: %s\t\tHash: %s\t\tTarget: %s\n", password, salt, hash, target);
         exit(1);
       } else printf("Trying %s...\r", password);
